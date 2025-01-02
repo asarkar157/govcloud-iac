5c01a6f76848fce7bad5e069aa8cb6b2b8d59882
stackgen-automation <automation@stackgen.com>
2025-01-02 22:07:18 +0000

    Automated commit

---
+++ iac/terraform/outputs.tf
@@ -0,0 +1,10 @@
+
+output "aws_iam_role_test_role_arn" {
+  value = module.stackgen_9fd33e04-081f-4256-94d0-18faaf1831de.arn
+  sensitive = false
+}
+
+output "aws_iam_role_test_role_name" {
+  value = module.stackgen_9fd33e04-081f-4256-94d0-18faaf1831de.name
+  sensitive = false
+}
